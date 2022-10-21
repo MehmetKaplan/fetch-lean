7bd2b062fc50a94dcdfdec0af02718d6b0c08d61
Mehmet Kaplan <mehmet.kaplan@gmail.com>
2022-10-21 23:00:19 +0300

    json() exists and non exists cases are handled.

--- fetch-lean.js
+++ fetch-lean.js
@@ -7,15 +7,21 @@ function fetch_add_params(p_params_as_json) {
 }
 
 const fetchLean = (p_method, p_uri, /* istanbul ignore next */ p_extra_headers = {}, /* istanbul ignore next */ p_body = {},) => new Promise(async (resolve, reject) => {
+	let l_method = p_method.toUpperCase();
 	let l_uri = p_uri;
 	let l_headers = Object.assign({ Accept: 'application/json', 'Content-Type': 'application/json', }, p_extra_headers);
-	let l_init = { method: p_method, headers: l_headers, }
-	/* istanbul ignore if */ 
-	if (p_method === "POST") l_init.body = JSON.stringify(p_body);
+	let l_init = { method: l_method, headers: l_headers, }
+	/* istanbul ignore if */
+	if (l_method === "POST") l_init.body = JSON.stringify(p_body);
 	else l_uri += "?" + fetch_add_params(p_body);
 	try {
 		let l_response = await fetch(l_uri, l_init);
-		return resolve(l_response);
+		try {
+			let l_response_json = await l_response.json();
+			return resolve(l_response_json);
+		} catch (error) {
+			return resolve(l_response);
+		}
 	} catch (error)  /* istanbul ignore next */ {
 		return reject(error)
 	}
